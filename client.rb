cb72f03e630196287a0ac37c3681074f8fcf721a
Ian York <iyork28@gmail.com>
2014-04-16 15:24:38 -0400

    Added dynamic "left" calculation for params hash

--- client.rb
+++ client.rb
@@ -13,7 +13,9 @@ class Client
     # test = HTTP.get @torrent["announce"], params: make_params_hash
     uri = URI(@torrent["announce"])
     uri.query = URI.encode_www_form make_params_hash
-    
+    # sum = 0
+    # @torrent["info"]["files"].each { |file| sum += file["length"] }
+    # puts sum
     res = Net::HTTP.get_response(uri)
     
     puts res
@@ -22,15 +24,18 @@ class Client
   private
   
   def make_params_hash
+    sum = 0
+    @torrent["info"]["files"].each { |file| sum += file["length"] }
     {
       info_hash:  URI::encode(Digest::SHA1.hexdigest @torrent["info"].bencode),
-      peer_id:    URI::encode(Mac.addr.tr(':','').concat "12345678"),
+      # peer_id:    URI::encode(Mac.addr.tr(':','').concat "12345678"),
+      peer_id:    "-IY0001-000000000001", 
       port:       6681,
       uploaded:   0,
       downloaded: 0,
-      left:       0,
+      left:       sum,
       compact:    0,
-      no_peer_id: 0 
+      no_peer_id: 0
     }
   end
   
